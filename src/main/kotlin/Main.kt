38d5604b468b3d6d31308636434c3fb8a4f04942
imsyf <alghozalyi@gmail.com>
2022-06-12 08:49:22 +0700

    Add sample code of a traditional backing field setup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Model {
+    private val _names: MutableList<String> = mutableListOf()
+    val names: List<String>
+        get() = _names
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    fun addName(name: String) {
+        _names.add(name)  // has access to `add` method
+    }
+}
+
+fun main(args: Array<String>) {
+    val model = Model()
+
+    // model.names.add("Kevin")  // doesn't have access to `add` method, should crash here
+    // println(model.names)
+
+    model.addName("Kevin")   // this is how it can be done
+    println(model.names)
 }
